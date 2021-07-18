593394979876498e3395fd572fd85f401abda87d
andipurwanto <andipurwanto912@gmail.com>
2021-07-18 23:27:57 +0700

    activity riwayat

--- app/src/main/java/andi/purwanto/retribusi_android/MasyarakatActivity.kt
+++ app/src/main/java/andi/purwanto/retribusi_android/MasyarakatActivity.kt
@@ -1,26 +1,21 @@
-//package andi.purwanto.retribusi_android
-//
-//import andi.purwanto.retribusi_android.adapters.MasyarakatActivityAdapter
-//import andi.purwanto.retribusi_android.contracts.MasyarakatActivityContract
-//import andi.purwanto.retribusi_android.databinding.ActivityLoginBinding.inflate
-//import andi.purwanto.retribusi_android.models.Masyarakat
-//import android.os.Binder
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.os.PersistableBundle
-//import android.view.View
-//import android.view.View.inflate
-//import android.widget.Toast
-//import androidx.recyclerview.widget.LinearLayoutManager
-//
-//class MasyarakatActivity : AppCompatActivity(), MasyarakatActivityContract.MasyarakatView {
-//
-//    private lateinit var binding: MasyarakatActivity
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = MasyarakatActivity.inflate(layoutInflater)
-//        val view = binding.root
-//        setContentView(view)
-//    }
-//}
+package andi.purwanto.retribusi_android
+
+import andi.purwanto.retribusi_android.adapters.MasyarakatActivityAdapter
+import andi.purwanto.retribusi_android.contracts.MasyarakatActivityContract
+import andi.purwanto.retribusi_android.databinding.ActivityLoginBinding.inflate
+import andi.purwanto.retribusi_android.models.Masyarakat
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.view.View.inflate
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class MasyarakatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_masyarakat)
+    }
+}
--- app/src/main/java/andi/purwanto/retribusi_android/ProfileActivity.kt
+++ app/src/main/java/andi/purwanto/retribusi_android/ProfileActivity.kt
@@ -17,5 +17,5 @@ class ProfileActivity : AppCompatActivity() {
      override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_profile)
-        }
+     }
 }
