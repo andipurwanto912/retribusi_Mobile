b9aca0b44cb66edef510d61dd9c00d11a7589678
andipurwanto <andipurwanto912@gmail.com>
2021-07-18 23:39:03 +0700

    seri

---
+++ app/src/main/java/andi/purwanto/retribusi_android/SeriActivity.kt
@@ -0,0 +1,11 @@
+package andi.purwanto.retribusi_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seri)
+    }
+}
