e0c991a29f6eb2c1f603e6d1f8641f4ac8a53172
andipurwanto <andipurwanto912@gmail.com>
2021-07-18 15:04:11 +0700

    profile activity

--- app/src/main/java/andi/purwanto/retribusi_android/MainMenuActivity.kt
+++ app/src/main/java/andi/purwanto/retribusi_android/MainMenuActivity.kt
@@ -1,11 +1,31 @@
 package andi.purwanto.retribusi_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
-class MainMenuActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var imageviewIntent: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        imageviewIntent = findViewById(R.id.profile)
+        imageviewIntent.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.profile->{
+                val intentProfileActivity = Intent(this@MainMenuActivity, ProfileActivity::class.java)
+            startActivity(intentProfileActivity)
+            }
+        }
     }
 }
---
+++ app/src/main/java/andi/purwanto/retribusi_android/MasyarakatActivity.kt
@@ -0,0 +1,11 @@
+package andi.purwanto.retribusi_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MasyarakatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_masyarakat)
+    }
+}
