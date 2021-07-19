ef4ec38965f0d44330be74a59edc42e803a1b0c4
andipurwanto <andipurwanto912@gmail.com>
2021-07-19 20:14:56 +0700

    error profile

--- app/src/main/java/andi/purwanto/retribusi_android/ProfileActivity.kt
+++ app/src/main/java/andi/purwanto/retribusi_android/ProfileActivity.kt
@@ -8,14 +8,47 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import andi.purwanto.retribusi_android.LoginActivity
 import andi.purwanto.retribusi_android.R
+import andi.purwanto.retribusi_android.contracts.LoginActivityContract
+import andi.purwanto.retribusi_android.databinding.ActivityLoginBinding
 import andi.purwanto.retribusi_android.models.User
 import andi.purwanto.retribusi_android.utilities.APIClient
 import andi.purwanto.retribusi_android.utilities.Constants
 import com.google.gson.Gson
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), LoginActivityContract.LoginView {
+
+    private lateinit var binding : ActivityLoginBinding
+//    private var presenter : LoginActivityContract.LoginPresenter? = null
+//    private lateinit var adapterMasyarakat : MasyarakatActivityAdapter
+
      override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_profile)
+         setContentView(R.layout.activity_profile)
+         buttonClick()
+         getUserData()
      }
-}
+
+    private fun buttonClick(){
+        binding.btnKeluar.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        Constants.clearToken(requireActivity())
+        startActivity(Intent(activity, LoginActivity::class.java).also {
+            activity?.finish()
+        })
+    }
+
+        private fun getUserData() {
+            var userData = Constants.getList(requireActivity())
+            var gson = Gson()
+
+            var user = gson.fromJson(userData, User::class.java)
+
+            binding.tvName.text = user.nama_lengkap
+            binding.tvEmail.text = user.email
+            binding.tvPhone.text = user.no_hp
+        }
+    }
